fc659042408d24d41287c19135156fcb5f41bcb8
ohin03 <nfohin@gmail.com>
2026-04-18 13:23:02 +0600

    final single render deploy

--- backend/server.js
+++ backend/server.js
@@ -220,6 +220,20 @@ app.use((err, req, res, next) => {
 
 
 
+
+
+
+
+
+
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
+
+
 /* =========================
    START SERVER
 ========================= */
