7c828333466b3e6b0f51939492638ca067c3ebd8
ohin03 <nfohin@gmail.com>
2026-04-18 14:03:52 +0600

    fix frontend build render issue

--- backend/server.js
+++ backend/server.js
@@ -220,22 +220,6 @@ app.use((err, req, res, next) => {
 
 
 
-
-
-
-/* =========================
-   REACT BUILD SERVE (FIXED)
-========================= */
-
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
-});
-
-
-
-
 /* =========================
    START SERVER
 ========================= */
